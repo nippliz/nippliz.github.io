e666e9130d6ef7c75a10d733b838f439c4a8cfa5
ipamaslo <ipamaslo@gmail.com>
2024-02-01 20:43:00 +0600

    updated

---
+++ script/main.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('bgimage'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './lottie/blob.json'
+});
